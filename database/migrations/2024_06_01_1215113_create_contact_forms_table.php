7ffba89eada4d14b27f022f11134d26727d4404c
snoussigaith <snoussi_gaith95@outlook.fr>
2024-06-03 19:00:41 +0100

    reglage contact

--- database/migrations/2024_06_01_1215113_create_contact_forms_table.php
+++ database/migrations/2024_06_01_1215113_create_contact_forms_table.php
@@ -16,6 +16,7 @@ return new class extends Migration {
             $table->string('address');
             $table->string('email');
             $table->string('service');
+            $table->string('object');
             $table->text('note');
             $table->timestamps();
         });
